902f73968ff72c6a5b0ae8be9e70ef6add17b71f
ashurik522 <ashurik522@gmail.com>
2022-06-10 10:13:36 -0400

    through directional functionality

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -14,34 +14,72 @@ const initialState = {
 }
 
 export default class AppClass extends React.Component {
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
-
-  getXY = () => {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
+ 
+  state = {
+    totalSteps: initialSteps,
+    xCoordinate: 2,
+    yCoordinate: 2,
+    board: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    currentIndex: initialIndex,
+    message: initialMessage,
+    email: initialEmail,
   }
+  
+  handleUp = (index) => {
+    const newBoard = [...this.state.board];
+    newBoard[index] = this.state.currentIndex;
+    this.setState({
+      ...this.state,
+      yCoordinate: this.state.yCoordinate > 1 ? this.state.yCoordinate - 1 : this.state.yCoordinate,
+      message: this.state.yCoordinate > 1 ? "" : "You can't go up",
+      currentIndex: this.state.currentIndex > 2 ? this.state.currentIndex - 3 : this.state.currentIndex,
+      totalSteps: this.state.currentIndex > 2 ? this.state.totalSteps + 1 : this.state.totalSteps,
+      board: newBoard
+    })
+  };
 
-  getXYMessage = () => {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
-  }
+  handleDown = (index) => {
+    const newBoard = [...this.state.board];
+    newBoard[index] = this.state.currentIndex;
+    this.setState({
+      ...this.state,
+      yCoordinate: this.state.yCoordinate < 3 ? this.state.yCoordinate + 1 : this.state.yCoordinate,
+      message: this.state.yCoordinate < 3 ? "" : "You can't go down",
+      currentIndex: this.state.currentIndex < 6 ? this.state.currentIndex + 3 : this.state.currentIndex,
+      totalSteps: this.state.currentIndex < 6 ? this.state.totalSteps + 1 : this.state.totalSteps,
+      board: newBoard
+    })
+  };
 
-  reset = () => {
-    // Use this helper to reset all states to their initial values.
-  }
+  handleLeft = (index) => {
+    const newBoard = [...this.state.board];
+    newBoard[index] = this.state.currentIndex;
+    this.setState({
+      ...this.state,
+      xCoordinate: this.state.xCoordinate > 1 ? this.state.xCoordinate - 1 : this.state.xCoordinate,
+      message: this.state.xCoordinate > 1 ? "" : "You can't go left",
+      currentIndex: (this.state.currentIndex === 0 || this.state.currentIndex === 3 || this.state.currentIndex ===6) 
+        ? this.state.currentIndex : this.state.currentIndex - 1,
+      totalSteps: (this.state.currentIndex === 0 || this.state.currentIndex === 3 || this.state.currentIndex ===6) 
+        ? this.state.totalSteps: this.state.totalSteps + 1,
+      board: newBoard
+      })
+  };
 
-  getNextIndex = (direction) => {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
-  }
-
-  move = (evt) => {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
-  }
+  handleRight = (index) => {
+    const newBoard = [...this.state.board];
+    newBoard[index] = this.state.currentIndex;
+    this.setState({
+      ...this.state,
+      xCoordinate: this.state.xCoordinate < 3 ? this.state.xCoordinate + 1 : this.state.xCoordinate,
+      message: this.state.xCoordinate < 3 ? "" : "You can't go right",
+      currentIndex: (this.state.currentIndex === 2 || this.state.currentIndex === 5 || this.state.currentIndex ===8) 
+        ? this.state.currentIndex : this.state.currentIndex + 1,
+      totalSteps: (this.state.currentIndex === 2 || this.state.currentIndex === 5 || this.state.currentIndex === 8) 
+        ? this.state.totalSteps: this.state.totalSteps + 1,
+      board: newBoard
+      })
+  };
 
   onChange = (evt) => {
     // You will need this to update the value of the input.
@@ -56,26 +94,26 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({this.state.xCoordinate}, {this.state.yCoordinate})</h3>
+          <h3 id="steps">You moved {this.state.totalSteps} times</h3>
         </div>
         <div id="grid">
           {
-            [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
+            this.state.board.map(idx => (
+              <div key={idx} className={`square${idx === this.state.currentIndex ? ' active' : ''}`}>
+                {idx === this.state.currentIndex ? 'B' : null}
               </div>
             ))
           }
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
+          <button onClick={this.handleLeft} id="left">LEFT</button>
+          <button onClick={this.handleUp} id="up">UP</button>
+          <button onClick={this.handleRight} id="right">RIGHT</button>
+          <button onClick={this.handleDown} id="down">DOWN</button>
           <button id="reset">reset</button>
         </div>
         <form>
